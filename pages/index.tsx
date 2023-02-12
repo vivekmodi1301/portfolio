f09c213dc996c0560eef3da63a991a2363576f05
vivekmodi1301 <modivivek03@gmail.com>
2023-02-12 20:13:38 +0530

    Completion of experience section

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import styles from "@/styles/Home.module.css";
 import Header from "@/components/Header";
 import Hero from "@/components/Hero";
 import About from "@/components/About";
+import WorkExperience from "@/components/WorkExperience";
 
 export default function Home() {
   return (
@@ -21,7 +22,9 @@ export default function Home() {
         <About/>
       </section>
       {/* Experience  */}
-
+      <section id='experience' className="snap-center">
+        <WorkExperience/>
+      </section>
       {/* Skills  */}
 
       {/* Projects  */}
